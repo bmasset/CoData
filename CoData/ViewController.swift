3c7dfb55f147404ec0f87513437063de00792fac
bmasset <bmasset@nordnet.fr>
2019-07-10 12:59:18 +0200

    Initial Commit

---
+++ CoData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoData
+//
+//  Created by B Masset on 10/07/2019.
+//  Copyright © 2019 B Masset. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
