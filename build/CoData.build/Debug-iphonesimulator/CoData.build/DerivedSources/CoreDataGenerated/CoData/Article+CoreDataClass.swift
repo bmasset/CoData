b6e1ca1432ff04ad22189bcae33a564ee447a040
bmasset <bmasset@nordnet.fr>
2019-07-18 11:22:24 +0200

    Test

--- build/CoData.build/Debug-iphonesimulator/CoData.build/DerivedSources/CoreDataGenerated/CoData/Article+CoreDataClass.swift
+++ build/CoData.build/Debug-iphonesimulator/CoData.build/DerivedSources/CoreDataGenerated/CoData/Article+CoreDataClass.swift
@@ -2,7 +2,7 @@
 //  Article+CoreDataClass.swift
 //  
 //
-//  Created by B Masset on 17/07/2019.
+//  Created by B Masset on 18/07/2019.
 //
 //  This file was automatically generated and should not be edited.
 //
